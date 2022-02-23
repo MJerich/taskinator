e211451107a33774faf6bee5840314a00b965ea7
MJerich <MatthewJerich@gmail.com>
2022-02-23 10:20:51 -0700

    form values are now being added to the created task

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,24 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 function createTaskHandler(event) {
     event.preventDefault();
-    let taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(taskItemEl);
+
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
